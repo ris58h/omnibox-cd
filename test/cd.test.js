bedc9061d6d4ad9e362482a32ea274b254cf6d6c
Ilya Rodionov <ris58h@gmail.com>
2018-07-22 11:09:26 +0300

    Swap expected and actual in test.

--- test/cd.test.js
+++ test/cd.test.js
@@ -1,40 +1,40 @@
 const cd = require("../cd")
-const assert = require('assert').strict
+const assert = require("assert").strict
 
 describe("cd", () => {
     it("should resolve absolute path", () => {
-        assert.equal("http://8.8.8.8:80", cd("http://8.8.8.8:80/ws", "/"))
-        assert.equal("http://8.8.8.8:80/path", cd("http://8.8.8.8:80/ws", "/path"))
-        assert.equal("file:///", cd("file:///", "/"))
-        assert.equal("file:///path", cd("file:///", "/path"))
+        assert.equal(cd("http://8.8.8.8:8080/ws", "/"), "http://8.8.8.8:8080")
+        assert.equal(cd("http://8.8.8.8:8080/ws", "/path"), "http://8.8.8.8:8080/path")
+        assert.equal(cd("file:///", "/"), "file:///")
+        assert.equal(cd("file:///", "/path"), "file:///path")
     })
 
     it("should resolve simple relative path", () => {
-        assert.equal("http://8.8.8.8:80/ws", cd("http://8.8.8.8:80/ws", "./"))
-        assert.equal("http://8.8.8.8:80/ws", cd("http://8.8.8.8:80/ws#hash", "./"))
-        assert.equal("http://8.8.8.8:80/ws", cd("http://8.8.8.8:80/ws?query", "./"))
-        assert.equal("http://8.8.8.8:80/ws/path", cd("http://8.8.8.8:80/ws", "./path"))
-        assert.equal("http://8.8.8.8:80/ws/path", cd("http://8.8.8.8:80/ws#hash", "./path"))
-        assert.equal("http://8.8.8.8:80/ws/path", cd("http://8.8.8.8:80/ws?query", "./path"))
+        assert.equal(cd("http://8.8.8.8:8080/ws", "./"), "http://8.8.8.8:8080/ws")
+        assert.equal(cd("http://8.8.8.8:8080/ws#hash", "./"), "http://8.8.8.8:8080/ws")
+        assert.equal(cd("http://8.8.8.8:8080/ws?query", "./"), "http://8.8.8.8:8080/ws")
+        assert.equal(cd("http://8.8.8.8:8080/ws", "./path"), "http://8.8.8.8:8080/ws/path")
+        assert.equal(cd("http://8.8.8.8:8080/ws#hash", "./path"), "http://8.8.8.8:8080/ws/path")
+        assert.equal(cd("http://8.8.8.8:8080/ws?query", "./path"), "http://8.8.8.8:8080/ws/path")
     })
 
     it("should resolve 'level up' relative path", () => {
-        assert.equal("http://8.8.8.8:80", cd("http://8.8.8.8:80/ws", ".."))
-        assert.equal("http://8.8.8.8:80", cd("http://8.8.8.8:80/ws#hash", ".."))
-        assert.equal("http://8.8.8.8:80", cd("http://8.8.8.8:80/ws?query", ".."))
-        assert.equal("http://8.8.8.8:80/path", cd("http://8.8.8.8:80/ws", "../path"))
-        assert.equal("http://8.8.8.8:80", cd("http://8.8.8.8:80/ws", "../.."))
+        assert.equal(cd("http://8.8.8.8:8080/ws", ".."), "http://8.8.8.8:8080")
+        assert.equal(cd("http://8.8.8.8:8080/ws#hash", ".."), "http://8.8.8.8:8080")
+        assert.equal(cd("http://8.8.8.8:8080/ws?query", ".."), "http://8.8.8.8:8080")
+        assert.equal(cd("http://8.8.8.8:8080/ws", "../path"), "http://8.8.8.8:8080/path")
+        assert.equal(cd("http://8.8.8.8:8080/ws", "../.."), "http://8.8.8.8:8080")
     })
 
     it("should resolve complex relative path", () => {
-        assert.equal("http://8.8.8.8:80/path", cd("http://8.8.8.8:80/ws", "/path/../path"))
-        assert.equal("http://8.8.8.8:80/path", cd("http://8.8.8.8:80/ws", "./path/../../path"))
-        assert.equal("http://8.8.8.8:80/path", cd("http://8.8.8.8:80/ws", "../path/../path"))
+        assert.equal(cd("http://8.8.8.8:8080/ws", "/path/../path"), "http://8.8.8.8:8080/path")
+        assert.equal(cd("http://8.8.8.8:8080/ws", "./path/../../path"), "http://8.8.8.8:8080/path")
+        assert.equal(cd("http://8.8.8.8:8080/ws", "../path/../path"), "http://8.8.8.8:8080/path")
     })
 
     it("should preserve trailing slash in path", () => {
-        assert.equal("https://ftp.mozilla.org/pub/", cd("https://ftp.mozilla.org/favicon.ico", "/pub/"))
-        assert.equal("https://ftp.mozilla.org/pub/firefox/", cd("https://ftp.mozilla.org/pub/", "./firefox/"))
-        assert.equal("https://ftp.mozilla.org/pub/", cd("https://ftp.mozilla.org/pub/firefox/", "../"))
+        assert.equal(cd("https://ftp.mozilla.org/favicon.ico", "/pub/"), "https://ftp.mozilla.org/pub/")
+        assert.equal(cd("https://ftp.mozilla.org/pub/", "./firefox/"), "https://ftp.mozilla.org/pub/firefox/")
+        assert.equal(cd("https://ftp.mozilla.org/pub/firefox/", "../"), "https://ftp.mozilla.org/pub/")
     })
 })
